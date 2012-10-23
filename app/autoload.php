42530f8080337447d3b968292d1818b10145c111
Enrico <enrico@gayalab.it>
2012-10-23 11:54:46 +0200

    Inizio git

---
+++ app/autoload.php
@@ -0,0 +1,24 @@
+<?php
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+// intl
+if (!function_exists('intl_get_error_code')) {
+    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+
+    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
+}
+
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+
+
+// app/autoload.php
+
+$loader->registerNamespaces(array(
+    'Admingenerator'    => array(__DIR__.'/../src', __DIR__.'/../vendor/bundles'),
+));
+
+return $loader;
